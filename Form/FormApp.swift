281e03cf727b179b2ec6a41f2fb149ba83a3ee8a
Hugo Pinheiro <hugo_hp10@hotmail.com>
2022-09-26 23:19:04 -0300

    Initial Commit

---
+++ Form/FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormApp.swift
+//  Form
+//
+//  Created by HP on 26/09/22.
+//
+
+import SwiftUI
+
+@main
+struct FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
