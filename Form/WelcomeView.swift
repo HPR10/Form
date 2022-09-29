9cbbd1f5d455f136c25187d55298799f0666e340
Hugo Pinheiro <hugo_hp10@hotmail.com>
2022-09-29 01:21:26 -0300

    add buttons in UI

--- Form/WelcomeView.swift
+++ Form/WelcomeView.swift
@@ -12,20 +12,40 @@ struct WelcomeView: View {
                 Text("WELCOME")
                     .foregroundColor(.purple)
                     .font(.system(size: 40, weight: .bold, design: .rounded))
-                    .offset(y: -280)
+                    .offset(y: -190)
                 Text("ON BOARD")
                     .foregroundColor(.white)
                     .font(.system(size: 30, weight: .medium))
-                    .offset(y: -280)
+                    .offset(y: -190)
                 Text("All the fun starts here! \n Wanna fell these happy vibes? \n Create an account and \n you all set to go!")
                     .foregroundColor(.white)
                     .multilineTextAlignment(.center)
-                    .offset(y: -250)
+                    .offset(y: -150)
                 
-                Button("SIGN UP") {
-                    .offset(y: 200)
-                }
+                Button {
+                    // sign up
+                } label: {
+                    Text("SIGN UP")
+                        .bold()
+                        .frame(width: 300, height: 70)
+                        .foregroundColor(.purple)
+                        .background(
+                            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                                .fill(Color.white)
+                        )
+                    }
+                .padding(.top)
+                .offset(y: 240)
                 
+                Button {
+                    // login in
+                } label: {
+                    Text("Have an account? Cool! \n Log in then!")
+                        .bold()
+                        .foregroundColor(.white)
+                    }
+                .padding(.top)
+                .offset(y: 240)
                 }
             }
         .ignoresSafeArea()
